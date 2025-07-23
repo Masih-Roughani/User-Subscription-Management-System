03b5235fb1b11e660d160bb7bb80ce53a8065a20
Masih-Roughani <masihroughani2005@gmail.com>
2025-07-23 14:41:03 +0330

    feat(Logic): add four functions in class SubscriptionManager
    add four functions in class SubscriptionManager:
    1. addCustomer
    2. addService
    3. subscribe
    4. cancelSubscription

--- Project/src/SubscriptionManager.java
+++ Project/src/SubscriptionManager.java
@@ -7,10 +7,26 @@ import java.util.List;
 @Data
 public class SubscriptionManager {
     private static final SubscriptionManager instance = new SubscriptionManager();
-    private final List<Customer> customers = new ArrayList<Customer>();
+    private final List<Customer> customers = new ArrayList<>();
     private final List<Service> services = new ArrayList<>();
     private final List<Subscription> subscriptions = new ArrayList<>();
 
     private SubscriptionManager() {
+    }
+
+    private void addCustomer(String fullName, String email) {
+        customers.add(new Customer(fullName, email));
+    }
+
+    private void addService(String name, String description) {
+        services.add(new Service(name, description));
+    }
+
+    private void subscribe(Customer customer, Service service) {
+        subscriptions.add(new Subscription(customer, service));
+    }
+
+    private void cancelSubscription(int subscriptionId) {
+        subscriptions.remove(subscriptionId);
     }
 }
